952afc0bd6e36dd94fa6e8f76ad7408f527c049b
sadrisso <sadriisso304@gmail.com>
2024-12-07 20:41:18 +0600

    get single data by filtering _id

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,9 +37,18 @@ async function run() {
         res.send(result)
     })
 
+
     app.get("/reviews", async (req, res) => {
         const query = await myCollection.find().toArray();
         res.send(query);
+    })
+
+
+    app.get("/review/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const result = await myCollection.findOne(filter)
+        res.send(result)
     })
 
 
